a4f74c2765b94bee5f7d200d1240440810f5bf17
annaapelsin <annadahlgren3@gmail.com>
2013-04-24 10:34:55 +0200

    Changes for mom5

--- main.php
+++ main.php
@@ -3,17 +3,17 @@
 <head>
   <meta charset='utf-8'/>
   <title>Anna Dahlgren</title>
-	<link rel='shortcut icon' href='http://www.student.bth.se/~ankw11/phpmvc/mom4/img/IMG_6504.JPG'/>
-  <link rel='stylesheet' href='http://www.student.bth.se/~ankw11/phpmvc/mom4/themes/core/style.css'/>
+	<link rel='shortcut icon' href='http://www.student.bth.se/~ankw11/phpmvc/mom5/img/IMG_6504.JPG'/>
+  <link rel='stylesheet' href='http://www.student.bth.se/~ankw11/phpmvc/mom5/themes/core/style.css'/>
 </head>
 <body>
   <div id='wrap-header'>
     <div id='header'>
     <div id='login-menu'>
-        <nav id='login-menu'><a href='http://www.student.bth.se/~ankw11/phpmvc/mom4/user/login'>login</a> </nav>      </div>
+        <nav id='login-menu'><a href='http://www.student.bth.se/~ankw11/phpmvc/mom5/user/login'>login</a> </nav>      </div>
     <div id='banner'>
      <a href='http://www.student.bth.se/~ankw11/phpmvc/mom2/'>
-        <img class='site-logo' src='http://www.student.bth.se/~ankw11/phpmvc/mom4/img/IMG_6504.JPG' alt='logo' width='80' height='80' />
+        <img class='site-logo' src='http://www.student.bth.se/~ankw11/phpmvc/mom5/img/IMG_6504.JPG' alt='logo' width='80' height='80' />
       </a>
       <p class='site-title'>phpmvc</p>
       <p class='site-slogan'>Mina jobb med php och mvc</p>
@@ -45,7 +45,7 @@ Ha det s&aring; bra!<br/>
 /Anna</p>
 
 
-<img src="../mom4/img/IMG_8455.JPG" alt = "Hem" width = "250" hight = "250"> </img>
+<img src="../mom5/img/IMG_8455.JPG" alt = "Hem" width = "250" hight = "250"> </img>
    
     </div>
   </div>
--- themes/core/functions.php
+++ themes/core/functions.php
@@ -6,16 +6,17 @@
 $ly->data['title'] = 'phpmvc';
 $ly->data['header'] = 'phpmvc';
 $ly->data['slogan'] = 'Mina jobb med php och mvc';
-$ly->data['favicon']      = 'http://www.student.bth.se/~ankw11/phpmvc/mom3/img/IMG_6504.JPG';
-$ly->data['logo']         = 'http://www.student.bth.se/~ankw11/phpmvc/mom3/img/IMG_6504.JPG';
+$ly->data['favicon']      = 'http://www.student.bth.se/~ankw11/phpmvc/mom5/img/IMG_6504.JPG';
+$ly->data['logo']         = 'http://www.student.bth.se/~ankw11/phpmvc/mom5/img/IMG_6504.JPG';
 $ly->data['logo_width']   = 80;
 $ly->data['logo_height']  = 80;
 $ly->data['footer']  = '<p>&copy; Anna Dahlgren</p>';
 $ly->data['navbar'] = <<<EOD
 <nav id = 'navbar'>
-<a href="http://www.student.bth.se/~ankw11/phpmvc/mom2/main.php">Me</a>
-<a href="http://www.student.bth.se/~ankw11/phpmvc/mom2/redovisning.php">Redovisning</a>
-<a href="http://www.student.bth.se/~ankw11/phpmvc/mom3/guestbook">G&auml;stbok</a>
+<a href="http://www.student.bth.se/~ankw11/phpmvc/mom5/main.php">Me</a>
+<a href="http://www.student.bth.se/~ankw11/phpmvc/mom5/redovisning.php">Redovisning</a>
+<a href="http://www.student.bth.se/~ankw11/phpmvc/mom5/guestbook">G&auml;stbok</a>
+<a href="http://www.student.bth.se/~ankw11/phpmvc/mom5/user/login">Logga in</a>
 <a href="source.php">K&auml;llkod</a>
 
 
